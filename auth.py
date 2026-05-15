16bd9154d8617ca45469eb61806bf6c892a0e57e
sam95154 <sameerbasha757@gmail.com>
2026-05-16 01:37:52 +0530

    Initial Commit

---
+++ auth.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+def login():
+
+    st.sidebar.title("🔐 Login")
+
+    username = st.sidebar.text_input(
+        "Username"
+    )
+
+    password = st.sidebar.text_input(
+        "Password",
+        type="password"
+    )
+
+    users = {
+        "sameer": "sameer123",
+        "admin": "admin123",
+        "guest": "guest123"
+    }
+
+    if username in users and users[username] == password:
+
+        st.session_state.username = username
+
+        st.sidebar.success(
+            f"Welcome {username} ✅"
+        )
+
+        return True
+
+    else:
+
+        st.sidebar.warning(
+            "Enter Correct Credentials"
+        )
+
+        return False
